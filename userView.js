cd7946b98bcbfd1a28695f336046519ef2fe8bf4
Joshua Frey <freyjt@gmail.com>
2016-01-09 22:51:01 -0500

    Added Gametoken object to userView.js

--- userView.js
+++ userView.js
@@ -6,84 +6,116 @@
 ////
 ////
 
-function UserView(canID) {
-
+function UserView(canID, initControlObject) {
+    this.control   = initControlObject;
     this.canvas    = document.getElementById(canID)
     this.context   = this.canvas.getContext("2d");
-    this.sizeX     = this.canvas.width;
-    this.sizeY     = this.canvas.height;
+    this.sizeX     = parseInt(this.canvas.width );
+    this.sizeY     = parseInt(this.canvas.height);
     this.gridSpace = ( this.sizeX < this.sizeY ) ? this.sizeX : this.sizeY;
     this.startX    = (this.sizeX - this.gridSpace) / 2;
     this.startY    = (this.sizeY -this.gridSpace) / 2;
 
     this.gridCount = 19;
-    this.gridSize  = this.gridSpace;
+    this.gridSize  = this.gridSpace / this.gridCount;
 
     ////dynamizeMe
     this.bgColor      = "#ffffff";
     this.playerColors = ['#ff00ff', '#00ff00'];
     this.gridColor    = "#000000";
 
+    var registrantThis = this;
+    this.canvas.addEventListener("mousemove", function(evt) {
 
+        pos = getMousePosition(evt);
+        pos = registrantThis.transMouseToGrid( pos );
+        console.log( pos );
+    });
 
 }
 
 
 //get dots from control
 // package model on server ;)
-UserView.prototype.render  = function( dots ) {
+UserView.prototype.render  = function( dots, turnDot ) {
 
-    this.context.fillColor = this.bgColor;
+    this.context.fillStyle = this.bgColor;
     this.context.fillRect(0, 0, this.sizeX, this.sizeY);
 
     this.drawGrid( );
     this.drawDots( dots );
+
 }
 
 
 UserView.prototype.drawGrid = function(  ) {
     this.context.strokeStyle = this.gridColor;
+    this.context.lineWidth   = 3; //exaggerated for visibility at the (literal) edge
     this.context.beginPath();
 
-    for(var i = 0; i < gridCount; i++) {
-    
-        this.context.moveTo(startX, startY + i * gridSize);
-        this.context.lineTo(sizeX - startX, startY + i * gridSize);
+    for(var i = 0; i < this.gridCount + 1; i++) {
+
+        this.context.beginPath();
+        this.context.moveTo(this.startX,              this.startY + i * this.gridSize);
+        this.context.lineTo(this.sizeX - this.startX, this.startY + i * this.gridSize);
         this.context.stroke();
 
-        this.context.moveTo(startX + i * gridSize , startY);
-        this.context.lineTo(startX + i * gridSize , sizeY - startY);
+        this.context.beginPath();
+        this.context.moveTo(this.startX + i * this.gridSize , this.startY);
+        this.context.lineTo(this.startX + i * this.gridSize , this.sizeY - this.startY);
+        console.log( this.gridSize );
         this.context.stroke();
-    
+
     }
+    this.context.stroke();
 }
 
-UserView.prototype.drawDots = function( dots ) {
+UserView.prototype.drawDots = function( dots, playerMove ) {
 
     //dots is an object of lists of touples
-    this.context.beginPath();
-    for(var i = 0; i < dots.length; i++) {
-        this.context.fillStyle = this.playerColors[i];
-        for(var j = 0; j < dots[i].length; j++) {
+    if( typeof(dots) !== 'undefined' ) {
+        this.context.beginPath();
+        for(var i = 0; i < dots.length; i++) {
+            this.context.fillStyle = this.playerColors[i];
+            for(var j = 0; j < dots[i].length; j++) {
+                /*
+                    
+                    CODE TO DRAW DOTS
 
+                */
+            }
         }
     }
+    if( typeof(playerMove) !== 'undefined' ) {
+
+
+    } 
+}
+UserView.prototype.drawToken        = function( tokenIn ) {
 
 }
+UserView.prototype.transMouseToGrid = function( mouse ) {
 
+    var halfGrid = this.gridSize / 2;
+    //this is true iff we have a square board @TODO we need to offset
+    //  mouse and size?
+    var X = Math.floor( this.gridCount * (mouse.x + halfGrid) / this.sizeX);
+    var Y = Math.floor( this.gridCount * (mouse.y + halfGrid) / this.sizeY);
 
+    return {x: X, y: Y};
+}
 
 function UserControl(   ) {
 
-    this.view = new UserView( 'gameCanvas');
+    this.view = new UserView( 'gameCanvas', this);
     this.view.render( );
-
+    this.io   = io();
     this.io.on('connection', function(socket) {
 
         socket.on('play made', function( dots ) {
 
-            dots = JSON.parse( dots );
-            view.render( dots );
+            this.dots = JSON.parse( dots );
+            view.render( this.dots );
 
         });
 
@@ -91,6 +123,76 @@ function UserControl(   ) {
 
 }
 
-window.onload = function( ) {
-    cont = new UserControl();
+
+function GameToken( xyObjectIn ) {
+
+    this.x = 0;
+    this.y = 0;
+
+    if( typeof( xyObjectIn) !== 'undefined' ) {
+        try {
+            if(typeof( xyObjectIn.x) !== 'undefined' &&
+                typeof( xyObjectIn.y) !== 'undefined') {
+                this.x = xyObjectIn.x;
+                this.y = xyObjectIn.y;
+            } else {
+                throw "assignError";
+            }
+        }
+        catch (err) {
+            console.log("Bad assignment in gameToken constructor.")
+        }
+    }
 }
+GameToken.prototype.setLocation = ( xyObjectIn ) => {
+        if( typeof( xyObjectIn) !== 'undefined' ) {
+        try {
+            if(typeof( xyObjectIn.x) !== 'undefined' &&
+                typeof( xyObjectIn.y) !== 'undefined') {
+                this.x = xyObjectIn.x;
+                this.y = xyObjectIn.y;
+            } else {
+                throw "assignError";
+            }
+        }
+        catch(err) {
+            console.log("Bad assignment in gameToken setLocation.")
+        }
+    }
+}
+GameToken.prototype.getLocation = ( ) => { return {x: this.x, y: this.y}; }
+
+
+
+window.onload = function( ) {
+    var cont = new UserControl();
+}
+
+
+
+
+
+
+
+
+////////////////////
+//http://www.kirupa.com/html5/getting_mouse_click_position.htm
+function getMousePosition(e) {
+
+    var parentPosition = getPosition(e.currentTarget);
+    var xPosition = e.clientX - parentPosition.x;
+    var yPosition = e.clientY - parentPosition.y;
+    return { x: xPosition, y: yPosition };
+    function getPosition(element) {
+        var xPosition = 0;
+        var yPosition = 0;
+          
+        while (element) {
+            xPosition += (element.offsetLeft - element.scrollLeft + element.clientLeft);
+            yPosition += (element.offsetTop - element.scrollTop + element.clientTop);
+            element = element.offsetParent;
+        }
+        return { x: xPosition, y: yPosition };
+    }
+}
+//END http://www.kirupa.com/html5/getting_mouse_click_position.htm
